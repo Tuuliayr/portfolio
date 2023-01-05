92461e606753102a988941d2f4e91506e5c876a4
Tuuliayr <tuuliayravainen@gmail.com>
2023-01-05 16:45:34 +0200

    Add icons.

--- src/components/myIntroduction/MyLinks.tsx
+++ src/components/myIntroduction/MyLinks.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import DownloadCV from './DownloadCV';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+//import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import './MyLinks.scss';
 
 const MyLinks: React.FC = () => {
@@ -8,9 +11,11 @@ const MyLinks: React.FC = () => {
     return (
         <Grid container rowSpacing={3} direction="column" className="my-links">
             <Grid item xs={12}>
-                <a href="https://www.youtube.com/" rel="noreferrer">LinkedIn</a>
+                <LinkedInIcon/>
+                <a href="https://www.linkedin.com/" rel="noreferrer">LinkedIn</a>
             </Grid>
             <Grid item xs={12}>
+                <GitHubIcon/>
                 <a href="https://github.com/Tuuliayr" rel="noreferrer">Github</a>
             </Grid>
             <Grid item xs={12}>
