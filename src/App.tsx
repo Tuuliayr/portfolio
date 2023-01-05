6b507aa0a90d8f22e56c7b2134ccf46b9d72d4d6
Tuuliayr <tuuliayravainen@gmail.com>
2023-01-05 16:26:41 +0200

    Create grids for introduction and projects.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
 import './App.css';
 import MyIntroduction from './components/myIntroduction/MyIntroduction';
 import Projects from './components/projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <MyIntroduction/>
-      <Projects/>
-    </div>
+      <Container maxWidth="xl" className="app">
+        <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12}>
+            <MyIntroduction/>
+          </Grid>
+          <Grid item xs={12}>
+            <Projects/>
+          </Grid>
+        </Grid>
+      </Container>
   );
 }
 
