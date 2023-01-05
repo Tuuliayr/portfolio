92872bca3448ce53fb9d20bb1d1f99681e1ce199
Tuuliayr <tuuliayravainen@gmail.com>
2023-01-05 16:47:10 +0200

    Editing layout.

--- src/components/myIntroduction/MyIntroduction.tsx
+++ src/components/myIntroduction/MyIntroduction.tsx
@@ -8,19 +8,19 @@ import './MyIntroduction.scss';
 const MyIntroduction: React.FC = () => {
     return (
         <div>
-            <Grid container columnSpacing={0} direction="row" className="my-introduction">
+            <Grid container direction="row" className="my-introduction">
                 <Grid item xs={3} className="grid-picture">
                     <div className="grid-picture__my-pic">
                         <MyPicture/>
                     </div>
                 </Grid>
-                <Grid item xs={6} className="grid-info-about-me">
+                <Grid item xs={7} className="grid-info-about-me">
                     <p className="grid-info-about-me__paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
                         dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
                         sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </Grid>
-                <Grid item xs={3} className="grid-links">
+                <Grid item xs={2} className="grid-links">
                     <div className="grid-links__important">
                         <MyLinks/>
                     </div>
