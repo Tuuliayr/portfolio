fec91c6dbce8d1c46b13d113c445f48ee615b034
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-02 14:51:14 +0200

    Add buttons for selecting translation.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import './App.css';
+import LanguageSelector from './components/languageSelector/LanguageSelector';
 import MyIntroduction from './components/myIntroduction/MyIntroduction';
 import Projects from './components/projects/Projects';
 import ContactMe from './components/contactMe/ContactMe';
@@ -9,8 +10,9 @@ import ContactMe from './components/contactMe/ContactMe';
 function App() {
   return (
       <Container maxWidth="xl" className="app">
-        <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid container rowSpacing={20} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           <Grid item xs={12}>
+            <LanguageSelector/>
             <MyIntroduction/>
           </Grid>
           <Grid item xs={12}>
