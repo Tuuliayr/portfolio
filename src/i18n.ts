326f4b25508ed08240ce9d62ba10a8bc977b6e29
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-02 14:02:49 +0200

    Add translation management.

---
+++ src/i18n.ts
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import fi from "./locales/fi.json";
+import en from "./locales/en.json";
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources: {
+            fi: { translation: fi },
+            en: { translation: en },
+        },
+        lng: i18n.language,
+        fallbackLng: "fi",
+
+        interpolation: {
+            escapeValue: false,
+        },
+        backend: {
+            loadPath: "/locales/{{lng}}.json",
+        },
+        react: {
+            bindI18n: "loaded languageChanges",
+            bindI18nStore: "added",
+            useSuspense: true,
+        },
+    });
+
+    export default i18n;
