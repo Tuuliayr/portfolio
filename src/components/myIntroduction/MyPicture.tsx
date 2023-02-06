58005331749a6d486b8014b8b1eb74f0219adaaa
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-06 15:55:48 +0200

    Change avatar.

--- src/components/myIntroduction/MyPicture.tsx
+++ src/components/myIntroduction/MyPicture.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import image from "../../static/images/cat.jpg";
+import image from "../../static/images/me.jpg";
 
 const MyPicture: React.FC = () => {
     return <Avatar alt="Tuuli" src={image} sx={{ width: 300, height: 300 }}/>
