31982e8507c88f6d36fe15f6d367900234210886
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-06 15:27:13 +0200

    Add props.

--- src/components/base/myButton/MyButton.tsx
+++ src/components/base/myButton/MyButton.tsx
@@ -4,16 +4,29 @@ import './myButton.scss';
 import { useTranslation } from 'react-i18next';
 
 interface Props {
-    type?: "default" | "pink" | "link" | "transparent";
+    type?: "default" | "link" | "secondary";
+    variant: "contained" | "text" | "outlined";
     label: string;
-    onClick: () => void;
+    onClick?: () => void;
+    submit?: boolean;
+    icon?: React.ReactNode;
+    link?: string;
+    size?: "small" | "medium" | "large";
 } 
 
-const MyButton: React.FC<Props> = ({ type = "default", label }) => {
+const MyButton: React.FC<Props> = ({ type = "default", variant, label, onClick, submit, icon, link, size="medium"}) => {
     const {t} = useTranslation();
 
     return (
-        <Button variant="contained" className={type}>
+        <Button 
+            variant={variant}
+            className={type}
+            onClick={onClick}
+            type={submit ? "submit" : "button"}
+            startIcon={icon}
+            href={link}
+            size={size}
+        >
             {t(label)}
         </Button>
     );
