ae35ecf9297b219365c3136c492f8a9567d43333
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-06 15:44:38 +0200

    Use MyButton component.

--- src/components/myIntroduction/MyLinks.tsx
+++ src/components/myIntroduction/MyLinks.tsx
@@ -4,19 +4,18 @@ import DownloadCV from './DownloadCV';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 //import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
-import './MyLinks.scss';
+import MyButton from '../base/myButton/MyButton';
+import './myLinks.scss';
 
 const MyLinks: React.FC = () => {
 
     return (
         <Grid container rowSpacing={3} direction="column" className="my-links">
             <Grid item xs={12}>
-                <LinkedInIcon/>
-                <a href="https://www.linkedin.com/" rel="noreferrer">LinkedIn</a>
+                <MyButton variant="text" type="link" label="LinkedIn" icon={<LinkedInIcon/>} link="https://www.linkedin.com/"></MyButton>
             </Grid>
             <Grid item xs={12}>
-                <GitHubIcon/>
-                <a href="https://github.com/Tuuliayr" rel="noreferrer">Github</a>
+                <MyButton variant="text" type="link" label="Github" icon={<GitHubIcon/>} link="https://github.com/Tuuliayr"></MyButton>
             </Grid>
             <Grid item xs={12}>
                 <DownloadCV/>
