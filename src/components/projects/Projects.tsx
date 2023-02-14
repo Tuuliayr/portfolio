02ed0f13416fe36123255af3def8ff26f7c3cad7
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-14 13:56:07 +0200

    Add title.

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import ProjectCard from './ProjectCard';
+import { useTranslation } from 'react-i18next';
 
 const Projects: React.FC = () => {
+
+    const {t} = useTranslation();
+
     return (
         <Grid container spacing={6}>
-            <Grid item xs={4}>
-                <ProjectCard/>
+            <Grid item xs={12}>
+                <h2>{t("projects.title")}</h2>
             </Grid>
             <Grid item xs={4}>
                 <ProjectCard/>
@@ -14,7 +18,9 @@ const Projects: React.FC = () => {
             <Grid item xs={4}>
                 <ProjectCard/>
             </Grid>
-
+            <Grid item xs={4}>
+                <ProjectCard/>
+            </Grid>
         </Grid>
     );
 }
