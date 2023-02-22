cd16c2112b053ec0eafa97ee0d820606cd15fd53
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-22 17:20:38 +0200

    Use flexbox.

--- src/components/myIntroduction/MyLinks.tsx
+++ src/components/myIntroduction/MyLinks.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import DownloadCV from './DownloadCV';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-//import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import MyButton from '../base/myButton/MyButton';
 import './myLinks.scss';
 
 const MyLinks: React.FC = () => {
 
     return (
-        <Grid container rowSpacing={3} direction="column" className="my-links">
-            <Grid item xs={12}>
-                <MyButton variant="text" type="link" label="LinkedIn" icon={<LinkedInIcon/>} link="https://www.linkedin.com/"></MyButton>
-            </Grid>
-            <Grid item xs={12}>
-                <MyButton variant="text" type="link" label="Github" icon={<GitHubIcon/>} link="https://github.com/Tuuliayr"></MyButton>
-            </Grid>
-            <Grid item xs={12}>
-                <DownloadCV/>
-            </Grid>
-        </Grid>
+        <div className="links-container">
+            <div className="links-linkedin">
+                <MyButton variant="text" type="link" label="LinkedIn" size="large" icon={<LinkedInIcon/>} link="https://www.linkedin.com/"></MyButton>
+            </div>
+            <div className="links-github">
+                <MyButton variant="text" type="link" label="Github" size="large" icon={<GitHubIcon/>} link="https://github.com/Tuuliayr"></MyButton>
+            </div>
+            <DownloadCV/>
+        </div>
     );
 }
 
