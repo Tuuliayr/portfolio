3d36224034d3741c10c063e1a543b6da3bc4150b
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-22 15:29:06 +0200

    Update.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import './App.css';
+import './App.scss';
 import LanguageSelector from './components/languageSelector/LanguageSelector';
 import MyIntroduction from './components/myIntroduction/MyIntroduction';
 import Projects from './components/projects/Projects';
 import ContactMe from './components/contactMe/ContactMe';
 
 function App() {
-  return (
-      <Container maxWidth="xl" className="app">
-        <Grid container rowSpacing={20} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-          <Grid item xs={12} className="top">
+    return (
+        <>
             <LanguageSelector/>
             <MyIntroduction/>
-          </Grid>
-          <Grid item xs={12}>
             <Projects/>
-          </Grid>
-          <Grid item xs={12}>
             <ContactMe/>
-          </Grid>
-        </Grid>
-      </Container>
-  );
+        </>
+    );
 }
 
 export default App;
