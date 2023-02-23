97c707bd918f0a4874a97af6253b8906d979a658
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-23 18:00:57 +0200

    Create footer.

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <p>Tuuli Äyräväinen 2023 | tuuliayravainen@gmail.com</p>
+        </footer>
+    );
+}
+
+export default Footer;
