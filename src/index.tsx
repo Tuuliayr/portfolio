f3cf11a3ce20ac9df5bc7e02651e746a0d64582c
Tuuliayr <tuuliayravainen@gmail.com>
2023-02-24 14:37:46 +0200

    Add navbar component.

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import "./i18n";
+import Navbar from './components/navbar/Navbar';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Navbar/>
     <App />
   </React.StrictMode>
 );
