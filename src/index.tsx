82591f971ed431d8689e91aacc9d835d87514126
Tuuliayr <tuuliayravainen@gmail.com>
2023-03-22 16:37:52 +0200

    Use MediaQuery for choosing what components to render based on the screen size.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MediaQuery from 'react-responsive';
 import App from './App';
 import "./i18n";
 import Navbar from './components/navbar/Navbar';
+import NavbarMobile from './components/navbar/NavbarMobile';
+import TopBarMobile from './components/navbar/TopBarMobile';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Navbar/>
+    <MediaQuery maxWidth={991}>
+        <TopBarMobile/>
+        <NavbarMobile/>
+    </MediaQuery>
+    <MediaQuery minWidth={992}>
+        <Navbar/>
+    </MediaQuery>
     <App />
   </React.StrictMode>
 );
