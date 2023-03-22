13b359dfe44eb1abfe2b0b3b532d521800194a63
Tuuliayr <tuuliayravainen@gmail.com>
2023-03-22 18:47:14 +0200

    Remove texts, add icons.

--- src/components/navbar/NavbarMobile.tsx
+++ src/components/navbar/NavbarMobile.tsx
@@ -10,36 +10,42 @@ const NavbarMobile: React.FC = () => {
         <BrowserRouter>
             <div className="navbar-mobile">
                 <div className="nav-links-mobile">
-                    <Link
-                        activeClass="active"
-                        to="aboutMe"
-                        smooth={true}
-                        spy={true}
-                        offset={-108}
-                        duration={500}
-                    >
-                        {t("navigation.about")}
-                    </Link>
-                    <Link
-                        activeClass="active"
-                        to="projects"
-                        smooth={true}
-                        spy={true}
-                        offset={-80}
-                        duration={500}
-                    >
-                        {t("navigation.work")}
-                    </Link>
-                    <Link
-                        activeClass="active"
-                        to="contactMe"
-                        smooth={true}
-                        spy={true}
-                        offset={-80}
-                        duration={500}
-                    >
-                        {t("navigation.contact")}
-                    </Link>
+                    <div className="nav-about">
+                        <Link
+                            activeClass="active"
+                            to="aboutMe"
+                            smooth={true}
+                            spy={true}
+                            offset={-108}
+                            duration={500}
+                        >
+                            {/* {t("navigation.about")} */}
+                        </Link>
+                    </div>
+                    <div className="nav-projects">
+                        <Link
+                            activeClass="active"
+                            to="projects"
+                            smooth={true}
+                            spy={true}
+                            offset={-48}
+                            duration={500}
+                        >
+                            {/* {t("navigation.work")} */}
+                        </Link>
+                    </div>
+                    <div className="nav-contact">
+                        <Link
+                            activeClass="active"
+                            to="contactMe"
+                            smooth={true}
+                            spy={true}
+                            offset={-48}
+                            duration={500}
+                        >
+                            {/* {t("navigation.contact")} */}
+                        </Link>
+                    </div>
                 </div>
             </div>
         </BrowserRouter>
