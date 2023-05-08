b097d98315e814eac82fec651e6eeeec69c0716c
Tuuliayr <tuuliayravainen@gmail.com>
2023-05-08 19:19:02 +0300

    Create pages for the projects.

---
+++ src/components/projects/projectPages/bakeryApp/BakeryApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BakeryApp: React.FC = () => {
+    return (
+        <div>
+            <h1>Bakery</h1>
+        </div>
+    )
+}
+
+export default BakeryApp;
---
+++ src/components/projects/projectPages/farmersApp/FarmersApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FarmersApp: React.FC = () => {
+    return (
+        <div>
+            <h1>Farmers</h1>
+        </div>
+    )
+}
+
+export default FarmersApp;
