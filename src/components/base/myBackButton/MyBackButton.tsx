4c79f34c98ecdcc099cec0febcb3be5bdeaae25c
Tuuliayr <tuuliayravainen@gmail.com>
2023-05-16 17:24:13 +0300

    Add translations for the text of the button.

--- src/components/base/myBackButton/MyBackButton.tsx
+++ src/components/base/myBackButton/MyBackButton.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from 'react-i18next';
 import ArrowBackIcon from '@mui/icons-material/ArrowBackIosNew';
 
 const MyBackButton = () => {
     const navigate = useNavigate();
+    const {t} = useTranslation();
     
     return (
         <button className="back-btn" onClick={() => navigate(-1)}>
             <ArrowBackIcon className="back-icon"/>
-            <p className="back-text">Back</p>
+            <p className="back-text">{t("buttons.back")}</p>
         </button>
     )
 }
