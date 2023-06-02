542a8e2c33297fe6c18b34735324537256252fb2
Tuuliayr <tuuliayravainen@gmail.com>
2023-06-02 14:04:11 +0300

    ,

--- src/components/myIntroduction/MyPicture.tsx
+++ src/components/myIntroduction/MyPicture.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import image from "../../static/images/me.jpg";
 
 const MyPicture: React.FC = () => {
-    return <img src={image} alt="Tuuli Äyräväinen" className="my-pic outline-border"/>
+    return <img src={image} alt="Tuuli" className="my-pic outline-border"/>
 }
 
 export default MyPicture;
