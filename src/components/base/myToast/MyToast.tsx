b2ac8108570787cf1bc665f67dbf57121d0fc463
Tuuliayr <tuuliayravainen@gmail.com>
2023-07-25 13:50:56 +0300

    Move toast function to ContactMe's sendForm result.

--- src/components/base/myToast/MyToast.tsx
+++ src/components/base/myToast/MyToast.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-// import { useTranslation } from 'react-i18next';
 import MyButton from '../myButton/MyButton';
   
 const MyToast: React.FC = () => {
-    // const {t} = useTranslation();
-
-    const notify = () => {
-        toast.success("Message sent!", {
-            position: toast.POSITION.TOP_CENTER
-        });
-    }
-
     return (
       <div>
         <MyButton
-            // submit
-            onClick={notify}
+            submit
             variant="contained"
             label="buttons.submit"
             size="large"
