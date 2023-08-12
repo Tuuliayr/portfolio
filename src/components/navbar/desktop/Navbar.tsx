aec59c8db121557c756e11390e1c9928e3b2528c
Tuuliayr <tuuliayravainen@gmail.com>
2023-08-12 14:27:19 +0300

    Split navbar elements into left and riht positioned elements. Toggle visibility of the elements based on current page.

--- src/components/navbar/desktop/Navbar.tsx
+++ src/components/navbar/desktop/Navbar.tsx
@@ -1,47 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router-dom';
 import LanguageSelector from '../../languageSelector/LanguageSelector';
+import MyBackButton from '../../base/myBackButton/MyBackButton';
 
 const Navbar: React.FC = () => {
     const {t} = useTranslation();
+    const [buttonIsVisible, setButtonIsVisible] = useState(false);
+    const [menuIsVisible, setMenuIsVisible] = useState(true);
+    const location = useLocation();
+
+    useEffect(() => {
+        if ( location.pathname === "/") {
+            setButtonIsVisible(false);
+            setMenuIsVisible(true);
+        } else {
+            setButtonIsVisible(true);
+            setMenuIsVisible(false);
+        }
+    }, [location.pathname]);
 
     return (
             <div className="navbar">
-                <div className="nav-links">
-                    <Link
-                        activeClass="active"
-                        to="aboutMe"
-                        smooth={true}
-                        spy={true}
-                        offset={-108}
-                        duration={500}
-                    >
-                        {t("navigation.about")}
-                    </Link>
-                    <Link
-                        activeClass="active"
-                        to="projects"
-                        smooth={true}
-                        spy={true}
-                        offset={-60}
-                        duration={500}
-                    >
-                        {t("navigation.work")}
-                    </Link>
-                    <Link
-                        activeClass="active"
-                        to="contactMe"
-                        smooth={true}
-                        spy={true}
-                        offset={-60}
-                        duration={500}
-                    >
-                        {t("navigation.contact")}
-                    </Link>
+                <div className="nav-left">
+                    {buttonIsVisible && <MyBackButton/>}
                 </div>
-                <div>
-                    <LanguageSelector/>
+                <div className="nav-right">
+                    {menuIsVisible &&
+                        <div className="nav-links">
+                            <Link
+                                activeClass="active"
+                                to="aboutMe"
+                                smooth={true}
+                                spy={true}
+                                offset={-108}
+                                duration={500}
+                            >
+                                {t("navigation.about")}
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="projects"
+                                smooth={true}
+                                spy={true}
+                                offset={-60}
+                                duration={500}
+                            >
+                                {t("navigation.work")}
+                            </Link>
+                            <Link
+                                activeClass="active"
+                                to="contactMe"
+                                smooth={true}
+                                spy={true}
+                                offset={-60}
+                                duration={500}
+                            >
+                                {t("navigation.contact")}
+                            </Link>
+                        </div>
+                    }
+                    <div>
+                        <LanguageSelector/>
+                    </div>
                 </div>
             </div>
     );
