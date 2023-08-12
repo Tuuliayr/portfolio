d7a3515f7039daf6483c35fb779a52fa6a71ca23
Tuuliayr <tuuliayravainen@gmail.com>
2023-08-12 16:12:19 +0300

    Rename styles.scss import.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./i18n";
-import "./Styles.scss";
+import "./styles.scss";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
