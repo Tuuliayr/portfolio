e92986565812e21cbddb3cd1b935cf83c2afd51d
Tuuliayr <tuuliayravainen@gmail.com>
2023-08-12 16:31:30 +0300

    Remove NavbarMobile.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useMediaQuery } from '@mui/material';
 import Navbar from './components/navbar/desktop/Navbar';
-import NavbarMobile from './components/navbar/mobile/NavbarMobile';
 import Main from './pages/mainPage/Main';
 import BakeryApp from './pages/projectPages/BakeryApp';
 import DanceVote from './pages/projectPages/DanceVote';
@@ -10,12 +8,9 @@ import FarmersApp from './pages/projectPages/FarmersApp';
 import ProjectBaltic from './pages/projectPages/ProjectBaltic';
 
 function App() {
-    const smallScreen = useMediaQuery('(max-width: 768px)');
-
     return (
         <BrowserRouter>
             <Navbar/>
-            {smallScreen && <NavbarMobile/>}
             <Routes>
                 <Route path="/" element={<Main/>}/>
                 <Route path="/bakery-app" element={<BakeryApp/>}/>
